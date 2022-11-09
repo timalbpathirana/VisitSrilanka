3c04258ae93728a26f61c22f822aa8a9dfad637e
Timal Pathirana <timalbathiya1995@gmail.com>
2022-11-09 13:12:09 +1100

    Added json handling

--- visitSrilanka/App/visitSrilankaApp.swift
+++ visitSrilanka/App/visitSrilankaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct visitSrilankaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
---
+++ visitSrilanka/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  visitSrilanka
+//
+//  Created by Timal Pathirana on 9/11/2022.
+//
+
+import Foundation
+
+struct CoverImage: Identifiable, Codable {
+let id: String
+let name: String
+}

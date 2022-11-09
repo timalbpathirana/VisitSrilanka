c2b295bb6a621eb03be27ea0fea6686539f2c726
Timal Pathirana <timalbathiya1995@gmail.com>
2022-11-09 11:59:02 +1100

    Initial Commit

---
+++ visitSrilanka/visitSrilankaApp.swift
@@ -0,0 +1,17 @@
+//
+//  visitSrilankaApp.swift
+//  visitSrilanka
+//
+//  Created by Timal Pathirana on 9/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct visitSrilankaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
